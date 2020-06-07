1a18418f109e88a91563ccdd6bc5994e6427980b
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-07 10:06:48 +0300

    added passport and a google strategy to handle auth with google

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
