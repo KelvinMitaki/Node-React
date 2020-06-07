865a7cd927dd04ae81f81bb64cbe6f9d82b9c114
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-07 11:46:59 +0300

    changed the folder structure for better scalability

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+require("./services/passport");
+const authRouter = require("./routes/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
+app.use(authRouter);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
