d22f9396cce3ce139902ab47df9e054ce6deb4c5
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-08 22:53:57 +0300

    added redux to the application to handle state

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose();
+const store = createStore(() => 1, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
