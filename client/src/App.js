c6eb29ae4ad3f6c89a72ed604a434cb664fd6b5b
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-08 21:33:29 +0300

    done some front end general clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a href="/auth/google">Sign In With Google</a>
-      </header>
+    <div>
+      <a href="/auth/google">Sign In With Google</a>
     </div>
   );
 }
