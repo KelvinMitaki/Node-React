67d173062d02f45f4b896c0334e66495c5feebb1
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-09 09:53:09 +0300

    added the header component

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
