a7752512c24528ec5576628a8d7ec73b7d3a2dba
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-09 20:28:02 +0300

    added the body parser middleware to parse all the incoming requests from the client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 require("./services/passport");
 const authRouter = require("./routes/authRoutes");
@@ -9,6 +10,7 @@ const authRouter = require("./routes/authRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
