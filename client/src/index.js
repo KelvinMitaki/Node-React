bc2070d1d251c71498d7d47a7f67b97f02e08e89
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-09 08:12:48 +0300

    aired up an auth reducer to the redux store

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
+import reducers from "./redux/reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose();
-const store = createStore(() => 1, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,8 +16,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
