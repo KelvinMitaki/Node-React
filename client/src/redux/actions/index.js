9fd340918e10305e2c27ee5cdc65b270cd8f9ddf
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-09 12:51:51 +0300

    added back the setup proxy due to some issues

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,8 +1,21 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, LOADING_START, LOADING_STOP } from "./types";
+
+export const loadingStart = () => {
+  return {
+    type: LOADING_START
+  };
+};
+
+export const loadingStop = () => {
+  return {
+    type: LOADING_STOP
+  };
+};
 
 export const fetchUser = () => async dispatch => {
+  loadingStart();
   const response = await axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: response });
-  console.log(response);
+  dispatch({ type: FETCH_USER, payload: response.data });
+  loadingStop();
 };
--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,1 +1,3 @@
 export const FETCH_USER = "FETCH_USER";
+export const LOADING_START = "LOADING_START";
+export const LOADING_STOP = "LOADING_STOP";
