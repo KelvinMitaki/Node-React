72fac99c18239e5cfdb77e69cb00e2c50c5c4ea7
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-10 14:39:09 +0300

    added the sendgrid boilerplate

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,24 @@
 const route = require("express").Router();
 
+const Survey = require("../models/Survey");
 const auth = require("../middlewares/authCheck");
 const credits = require("../middlewares/creditsCheck");
 
-route.post("/api/surveys", auth, credits, (req, res) => {});
+route.post("/api/surveys", auth, credits, async (req, res) => {
+  try {
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email })),
+      _user: req.user._id,
+      dateSent: Date.now()
+    });
+    await survey.save();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = route;
