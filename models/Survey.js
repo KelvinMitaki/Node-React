3286033a9b4c8e582ca21281615c174c810878bd
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-10 10:34:13 +0300

    added a receipient

--- models/Survey.js
+++ models/Survey.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
+const RecipientSchema = require("./Recipient");
+
 const SurveySchema = new mongoose.Schema({
   title: { type: String, required: true },
   subject: { type: String, required: true },
   body: { type: String, required: true },
-  recipients: { type: [String], required: true },
+  recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 }
 });
