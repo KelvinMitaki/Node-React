152ce96ca920fd9932c6446a208bc8f606c17eac
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-11 07:48:19 +0300

    added redirection if logged in

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 export class Landing extends Component {
   render() {
+    if (this.props.loggedIn) return <Redirect to="/surveys" />;
     return (
       <div style={{ textAlign: "center" }}>
         <h1>Emaily!</h1>
@@ -11,4 +14,10 @@ export class Landing extends Component {
   }
 }
 
-export default Landing;
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.loggedIn
+  };
+};
+
+export default connect(mapStateToProps)(Landing);
