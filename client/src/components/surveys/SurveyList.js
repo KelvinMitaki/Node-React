68ebe8ce8505ad7fd50eb5a52733705494295dee
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-12 07:39:03 +0300

    added rendering of surveys to the client

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,7 +7,21 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
   render() {
-    return <div></div>;
+    return this.props.surveys.surveys.map(survey => (
+      <div key={survey._id} className="card darken-1">
+        <div className="card-content">
+          <span className="card-title">{survey.title}</span>
+          <p>{survey.body}</p>
+          <p className="right">
+            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+          </p>
+        </div>
+        <div className="card-action">
+          <a>Yes: {survey.yes} </a>
+          <a>No: {survey.no} </a>
+        </div>
+      </div>
+    ));
   }
 }
 
