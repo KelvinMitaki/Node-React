35637d66abdbfa1a082ae514bfb0746893271179
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-17 20:02:50 +0300

    added expiration time to redis store

--- services/cache.js
+++ services/cache.js
@@ -36,7 +36,7 @@ mongoose.Query.prototype.exec = async function () {
       : new this.model(doc);
   }
   const result = await exec.apply(this, arguments);
-  client.hset(this._hashKey, key, JSON.stringify(result));
+  client.hset(this._hashKey, key, JSON.stringify(result), "EX", 10);
   console.log("from mongo db");
   return result;
 };
